36036a908b8d7a23a281392a1802b50c69d6ff11
Kevin Novak <kevinmnovak@yahoo.com>
2019-06-21 22:08:55 -0400

    Added support for 1_9_R1 and 1_9_R2

---
+++ craftbukkit_1_9_R1/src/main/java/me/kevinnovak/livecoordinates/v1_9_R1.java
@@ -0,0 +1,15 @@
+package me.kevinnovak.livecoordinates;
+
+import net.minecraft.server.v1_9_R1.IChatBaseComponent;
+import net.minecraft.server.v1_9_R1.PacketPlayOutChat;
+import org.bukkit.craftbukkit.v1_9_R1.entity.CraftPlayer;
+import org.bukkit.entity.Player;
+
+public class v1_9_R1 implements InternalsProvider {
+    @Override
+    public void sendActionBar(Player player, String message) {
+        IChatBaseComponent chatComponent = IChatBaseComponent.ChatSerializer.a("{\"text\":\"" + message + "\"}");
+        PacketPlayOutChat packet = new PacketPlayOutChat(chatComponent, (byte) 2);
+        ((CraftPlayer) player).getHandle().playerConnection.sendPacket(packet);
+    }
+}
