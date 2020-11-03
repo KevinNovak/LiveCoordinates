55b69477cda10b2df9578c9dfe84bdd3116fdb1a
Kevin Novak <kevinmichaelnovak@gmail.com>
2020-11-02 20:56:48 -0500

    1_16_R1, 1_16_R2, 1_16_R3 support

---
+++ craftbukkit_1_16_R1/src/main/java/me/kevinnovak/livecoordinates/v1_16_R1.java
@@ -0,0 +1,17 @@
+package me.kevinnovak.livecoordinates;
+
+import me.kevinnovak.livecoordinates.services.InternalsProvider;
+import net.minecraft.server.v1_16_R1.ChatMessageType;
+import net.minecraft.server.v1_16_R1.IChatBaseComponent;
+import net.minecraft.server.v1_16_R1.PacketPlayOutChat;
+import org.bukkit.craftbukkit.v1_16_R1.entity.CraftPlayer;
+import org.bukkit.entity.Player;
+
+public class v1_16_R1 implements InternalsProvider {
+    @Override
+    public void sendActionBar(Player player, String message) {
+        IChatBaseComponent chatComponent = IChatBaseComponent.ChatSerializer.a("{\"text\":\"" + message + "\"}");
+        PacketPlayOutChat packet = new PacketPlayOutChat(chatComponent, ChatMessageType.GAME_INFO, player.getUniqueId());
+        ((CraftPlayer) player).getHandle().playerConnection.sendPacket(packet);
+    }
+}
